3486097fc887fc80cddd1fa48ed6924cdd6a9ad1
Yvonne Cheng <cheng.yu.yvonne@gmail.com>
2024-06-21 19:25:04 +0000

    added unit tests

--- weather.py
+++ weather.py
@@ -2,10 +2,11 @@ import requests
 import pandas as pd
 import sqlalchemy as db
 
+
 def fetch_weather_data(api_key, city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    base_url = 'http://api.openweathermap.org/data/2.5/weather'
+    url = f'{base_url}?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
-    
     if response.status_code == 200:
         data = response.json()
         return {
@@ -16,18 +17,22 @@ def fetch_weather_data(api_key, city):
     else:
         raise Exception("Failed to retrieve data")
 
+
 def convert_to_dataframe(weather_data):
     return pd.DataFrame([weather_data])
+
 
 def save_to_database(df, db_name, table_name):
     engine = db.create_engine(f'sqlite:///{db_name}.db')
     df.to_sql(table_name, con=engine, if_exists='replace', index=False)
     return engine
 
+
 def query_database(engine, table_name):
     with engine.connect() as connection:
         results = connection.execute(db.text(f"SELECT * FROM {table_name};")).fetchall()
         return pd.DataFrame(results, columns=['City', 'Temperature', 'Weather'])
+
 
 if __name__ == "__main__":
     api_key = 'e9ade545133ca72b0db7d4ba4ef4200c'
