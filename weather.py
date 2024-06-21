522bba99719637f7c7eb66e2b3afacd687940862
Yvonne Cheng <cheng.yu.yvonne@gmail.com>
2024-06-21 18:36:28 +0000

    edited

--- weather.py
+++ weather.py
@@ -1,38 +1,36 @@
 import requests
-import pandas as pd 
+import pandas as pd
 import sqlalchemy as db 
 
 api_key = 'e9ade545133ca72b0db7d4ba4ef4200c'
-
 city = 'London'
-
-url = f'http://api.openweathermap.org/data/2.5/weather?
-    q={city}&appid=e9ade545133ca72b0db7d4ba4ef4200c&units=metric'
+base_url = 'http://api.openweathermap.org/data/2.5/weather'
+url = f'{base_url}?q={city}&appid={api_key}&units=metric'
 
 response = requests.get(url)
 
-#Check if request was successful
+# Check if request was successful
 if response.status_code == 200:
-  data = response.json()
-  weather_data = {
-    'City': [data['name']],
-    'Temperature': [data['main']['temp']],
-    'Weather': [data['weather'][0]['description']]
-  }
+    data = response.json()
+    weather_data = {
+        'City': [data['name']],
+        'Temperature': [data['main']['temp']],
+        'Weather': [data['weather'][0]['description']]
+    }
 else:
-  print("Failed to retrieve data.")
+    print("Failed to retrieve data.")
 
-#Convert dict into DataFrame
+# Convert dict into DataFrame
 df = pd.DataFrame.from_dict(weather_data)
 print(df)
 
-#Create engine object
+# Create engine object
 engine = db.create_engine('sqlite:///weather_data.db')
 
-#Create and send SQL table from DataFrame
+# Create and send SQL table from DataFrame
 df.to_sql('weather', con=engine, if_exists='replace', index=False)
 
-#Write query and print out requests
+# Write query and print out requests
 with engine.connect() as connection:
-  query_results = connection.execute(db.text("SELECT* FROM weather;")).fetchall()
-  print(pd.DataFrame(query_results, columns = ['City', 'Temperature', 'Weather']))
+    query_results = connection.execute(db.text("SELECT * FROM weather;")).fetchall()
+    print(pd.DataFrame(query_results, columns=['City', 'Temperature', 'Weather']))
